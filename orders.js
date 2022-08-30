d4e8a4b20b9ca9da1acf306fc525cb8c797bbfb8
Kyle Malone <Kylejaxonmalone93@gmail.com>
2022-08-29 20:44:24 -0500

    first commit

---
+++ orders.js
@@ -0,0 +1,36 @@
+// orders.js
+document.getElementById('specialInstructions')?.value
+const orders = [
+    {
+      id: 1,
+      crust: "Hand",
+      toppings: ["pepperoni", "green pepper"],
+      instructions: "extra cheese"
+    },
+    {
+      id: 2,
+      crust: "Thin",
+      toppings: ["Black Olives", "green pepper"],
+      instructions: "half green peppers"
+    }
+  ]
+  
+  export const getOrders = () => {
+    // Add logic here to return a copy of your orders
+    const copyOfOrders = orders.map ((order) => ({...order}));
+  }
+  
+  const getNewOrderId = () => {
+    let highestOrderId = orders.sort((a, b) => b.id - a.id)[0].id
+    return highestOrderId + 1
+  }
+  
+  export const addNewOrder = (order) => {
+    const newId = getNewOrderId()
+    // Add logic to give the order the property id, which is equal to newId
+    // Add logic to add the order object to the orders array
+  
+    // The below line creates and dispatches a new custom event titled "stateChanged".
+    // We will still need to add a listener for this event.
+    document.dispatchEvent(new CustomEvent("stateChanged"))
+  }
